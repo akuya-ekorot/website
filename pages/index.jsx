5a42a5b4727a03a01e6fe06e81699259ec91afae
Akuya Ekorot <akuyaekorot@gmail.com>
2022-04-18 01:53:36 +0300

    get blog data from Notion

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,46 +1,67 @@
 import BlogCard from "../components/BlogCard";
 import Footer from "../components/Footer";
 import Hero from "../components/Hero";
+import { Client } from "@notionhq/client";
 
-export default function Home() {
-  const blog1 = {
-    imageUrl: "/assets/1.jpg",
-    date: "17 Apr 2022",
-    title: "Still Staying Still",
-  };
-  const blog2 = {
-    imageUrl: "/assets/2.jpg",
-    date: "17 Apr 2022",
-    title: "Still Staying Still",
-  };
-
+export default function Home({ blogs }) {
   return (
-    <>
-      <div className="h-full w-full -z-10 fixed flex">
-        <div
-          className={
-            "w-full h-full absolute custom-backdrop-blur"
-          }
-        ></div>
-        <div className="w-2/4 h-2/4 bg-[#FC7753] custom-colors"></div>
-        <div className="w-2/4 h-2/4 bg-[#FFF689] custom-colors"></div>
-      </div>
-
-      <div className="max-w-[800px] flex flex-col gap-[60px] py-[60px] mx-auto z-10">
-        <Hero />
-        <div className="flex flex-col gap-5">
-          <h2 className="underline underline-offset-1 text-xl">
-            Explore some of my thoughts...
-          </h2>
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
-            <BlogCard blog={blog1} />
-            <BlogCard blog={blog2} />
-            <BlogCard blog={blog2} />
-            <BlogCard blog={blog2} />
-          </div>
+    <div className="max-w-[800px] flex flex-col gap-[60px] py-[60px] mx-auto z-10">
+      <Hero />
+      <div className="flex flex-col gap-5">
+        <h2 className="underline underline-offset-1 text-xl">
+          Explore some of my thoughts...
+        </h2>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
+          {blogs.map((blog) => {
+            return <BlogCard key={blog.id} blog={blog} />;
+          })}
         </div>
-        <Footer />
       </div>
-    </>
+      <Footer />
+    </div>
   );
+}
+
+export async function getStaticProps() {
+  const notion = new Client({ auth: process.env.NOTION_INTEGRATION_TOKEN });
+
+  const response = await notion.databases.query({
+    database_id: process.env.NOTION_DATABASE_ID,
+    filter: {
+      property: "Status",
+      select: {
+        equals: "Publish",
+      },
+    },
+  });
+
+  function getBlogsDetails(response) {
+    const blogs = [];
+    response?.results?.map((blog) => {
+      const date = new Date(
+        ...blog.properties["Date Published"].date.start.split("-")
+      ).toDateString();
+      const id = blog.id;
+      const imageUrl = blog.cover
+        ? blog.cover.external.url
+        : "/assets/avatar.jpg";
+      const title = blog.properties.Name.title[0].plain_text;
+
+      blogs.push({
+        date,
+        id,
+        imageUrl,
+        title,
+      });
+    });
+    return blogs;
+  }
+
+  const blogs = getBlogsDetails(response);
+
+  return {
+    props: {
+      blogs,
+    },
+  };
 }
