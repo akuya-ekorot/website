2e66fa653a4edb7a20e42f5ba57e50204f8157b3
Akuya Ekorot <akuyaekorot@gmail.com>
2022-04-17 21:07:58 +0300

    install tailwind

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,20 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <Head>
+        <title>Akuya Ekorot | Welcome</title>
+        <meta
+          name="description"
+          content="Welcome to my tiny corner on the internet 😊"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
